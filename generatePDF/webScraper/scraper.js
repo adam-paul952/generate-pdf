8e994865507a4474180a8fe504ad0c25f790e8d4
Adam Paul <adam.paul@live.com>
2022-08-06 09:51:53 -0230

    Included documentation for the scraper.js file

--- generatePDF/webScraper/scraper.js
+++ generatePDF/webScraper/scraper.js
@@ -1,7 +1,26 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+/**
+ * @file scraper.js
+ * @description Uses the web URL to pull in the HTML DOM elements and searches for the class="button" used as View Image
+ * @exports fetchAvailableImages
+ */
+
+/**
+ * @const url
+ * @type string
+ */
+
 const url = `https://newfoundlandpony.com/lineage/`;
+
+/**
+ * @async
+ * @function fetchAvailableImages
+ * @description axios makes the HTTP request to the webpage and cheerio acts the same as JQuery to traverse the DOM elements.
+ * The href from the link is '#popup###' so we use <string>.slice to just return the actual NPS number
+ * @returns { Promise.<string[]> } An array of pony NPS numbers who have an image associated with them on the lineage page.
+ */
 
 const fetchAvailableImages = async () => {
   return await axios(url)
